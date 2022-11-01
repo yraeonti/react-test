3cf203b4228d0d438eb9823b9fb3951b3c73508f
ibrahim sannu <prince.ibrahim76@gmail.com>
2022-11-01 11:20:20 +0100

    commit message

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -3,5 +3,5 @@ import React from 'react';
 function Error() {
 	return <div data-testid="errorMsg" className="alert error mt-20 slide-up-fade-in">Error Message</div>
 }
-
+// 
 export default Error;
